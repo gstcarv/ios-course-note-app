7fb57c222748c289e57108308063c4c7c715c9b9
Gustavo Carvalho <gstcarv@gmail.com>
2022-04-06 15:20:35 -0300

    feat: created note screen and saving on UserDefaults

--- SimpleNote/Controllers/ViewController.swift
+++ SimpleNote/Controllers/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var noteField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let savedData = UserDefaults.standard.string(forKey: "@saved_note") {
+            noteField.text = savedData;
+        }
     }
-
-
+    
+    @IBAction func onSaveNote(_ sender: UIButton) {
+        UserDefaults.standard.set(noteField.text, forKey: "@saved_note")
+        
+        let alert = UIAlertController(title: "Note saved", message: "The note was saved successfully", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
